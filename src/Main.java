dc73e738df9a99f99abe3d4b8e3fbcdcfb2c06d3
Shlomi <vgtvgy1@gmail.com>
2017-02-26 08:33:03 +0200

    My first commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+    public static void ABC() {
+
+    }
+
+    public static void bcd() {
 
     }
 }
---
+++ src/a.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Shlomi on 26/02/2017.
+ */
+public class a {
+    int a;
+}
