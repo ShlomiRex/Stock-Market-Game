d4f514df37900051ef7fa27fc1653d2382ecf3a3
Shlomi <vgtvgy1@gmail.com>
2017-02-26 18:09:26 +0200

    jn

---
+++ src/Company.java
@@ -0,0 +1,9 @@
+public class Company {
+    public String name;
+    public int share_price;
+
+    public Company(String name, int share_price) {
+        this.name = name;
+        this.share_price = share_price;
+    }
+}
